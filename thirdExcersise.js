9261452e03472246f5f74ed355423f56feaf51ee
Jakov Saric <jakov.saric96@gmail.com>
2023-04-24 17:08:40 +0200

    modified:   thirdExcersise.html 	modified:   thirdExcersise.js     updated to work as an extension

--- thirdExcersise.js
+++ thirdExcersise.js
@@ -5,27 +5,32 @@ let total = 0;
 let throws = [];
 
 showSelf();
-
+document.getElementById("d4El").addEventListener("click", d4El)
 function d4El() {
     roll(4);
 }
+document.getElementById("d6El").addEventListener("click", d6El)
 function d6El() {
     roll(6);
 }
+document.getElementById("d8El").addEventListener("click", d8El)
 function d8El() {
     roll(8);
 }
+document.getElementById("d10El").addEventListener("click", d10El)
 function d10El() {
     roll(10);
 }
+document.getElementById("d12El").addEventListener("click", d12El)
 function d12El() {
     roll(12);
 }
+document.getElementById("d20El").addEventListener("click", d20El)
 function d20El() {
     reset();
     roll(20);
 }
-
+document.getElementById("resetEl").addEventListener("click", reset)
 function reset() {
     throws.length = 0;
     total = 0;
